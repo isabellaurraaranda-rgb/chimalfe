b4554188549e22f6fa764ff871d4d2402065c1d7
isabellaurraaranda-rgb <isabellaurraaranda@gmail.com>
2026-02-16 17:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,28 @@ const STORE_CONFIG = {
 const PRODUCTS = {
     wool: [
         { id: 1, name: 'Huso de madera nativa - 53cm', price: 15000, image: 'huso.jpg' },
-        { id: 2, name: 'Lana Oveja Natural - 100g', price: 3800, image: 'lanadobrehebracrudo.jpg' },
-        { id: 3, name: 'Lana Flor de Aromo - 178g', price: 6400, image: 'lanafloraromo.jpg' },
-        { id: 4, name: 'Lana hilado artesanal - 500g', price: 22000, image: 'lanaartesanal.jpg' },
-        { id: 5, name: 'Lana de oveja pitio - 500g', price: 25000, image: 'lanapitio.jpg' }
+        { id: 2, name: 'Lana de oveja teñido con hoja de maqui - 500g', price: 27700, image: 'hojademaqui.jpg' },
+        { id: 3, name: 'Lana de oveja teñido con maqui - 500g', price: 27700, image: 'maqui.jpg' },
+        { id: 4, name: 'Lana Oveja Natural - 100g', price: 3800, image: 'lanadobrehebracrudo.jpg' },
+        { id: 5, name: 'Lana Flor de Aromo - 178g', price: 6400, image: 'lanafloraromo.jpg' },
+        { id: 6, name: 'Lana hilado artesanal - 500g', price: 22000, image: 'lanaartesanal.jpg' },
+        { id: 7, name: 'Lana de oveja pitio - 500g', price: 25000, image: 'lanapitio.jpg' }
     ],
     textiles: [
-        { id: 6, name: 'Amukan Makuñ 150*206cm', price: 420000, image: 'amukanmakun.jpg' },
-        { id: 7, name: 'Makuñ azul 145*168cm', price: 390000, image: 'makunazul.jpg' },
-        { id: 8, name: 'Faja Tejida con ñimin - 216cm Peso 497gr  ', price: 120000, image: 'fajanimin.jpg' },
-        { id: 9, name: 'Faja Roja - largo 265cm ', price: 95000, image: 'fajaroja.jpg' },
-        { id: 10, name: 'Bolso Artesanal 26*30cm', price: 35000, image: 'bolsoartesanal.jpg' },
-        { id: 11, name: 'Piecera tejida a telar 55*105cm', price: 50000, image: 'piecera.jpg' },
-        { id: 12, name: 'Cinturon de 180cm Técnica Pampa Rojo', price: 35000, image: 'trarilonkorojo.jpg' },
-        { id: 13, name: 'Cinturon de 180cm Técnica Pampa Azul', price: 35000, image: 'trarilonkoazul.jpg' }
+        { id: 8, name: 'Amukan Makuñ 150*206cm', price: 420000, image: 'amukanmakun.jpg' },
+        { id: 9, name: 'Makuñ azul 145*168cm', price: 390000, image: 'makunazul.jpg' },
+        { id: 10, name: 'Faja Tejida con ñimin - 216cm Peso 497gr  ', price: 120000, image: 'fajanimin.jpg' },
+        { id: 11, name: 'Faja Roja - largo 265cm ', price: 95000, image: 'fajaroja.jpg' },
+        { id: 12, name: 'Bolso Artesanal 26*30cm', price: 35000, image: 'bolsoartesanal.jpg' },
+        { id: 13, name: 'Piecera tejida a telar 55*105cm', price: 50000, image: 'piecera.jpg' },
+        { id: 14, name: 'Cinturon de 180cm Técnica Pampa Rojo', price: 35000, image: 'trarilonkorojo.jpg' },
+        { id: 15, name: 'Cinturon de 180cm Técnica Pampa Azul', price: 35000, image: 'trarilonkoazul.jpg' }
     ],
     classes: [
-        { id: 14, name: 'Clase CHAÑUNTUKU', price: 30000, image: 'tallervirtualchanuntuku.jpg' },
-        { id: 15, name: 'Clase MAKUÑ', price: 120000, image: 'virtualmakun.jpg' },
-        { id: 16, name: 'Clase TALEGA Bolso Andino', price: 40000, image: 'talega.jpg' },
-        { id: 17, name: 'Taller virtual de Metodologías sociales para proyectos artísticos y culturales', price: 40000, image: 'msac.jpg' }
+        { id: 16, name: 'Clase CHAÑUNTUKU', price: 30000, image: 'tallervirtualchanuntuku.jpg' },
+        { id: 17, name: 'Clase MAKUÑ', price: 120000, image: 'virtualmakun.jpg' },
+        { id: 18, name: 'Clase TALEGA Bolso Andino', price: 40000, image: 'talega.jpg' },
+        { id: 19, name: 'Taller virtual de Metodologías sociales para proyectos artísticos y culturales', price: 40000, image: 'msac.jpg' }
     ]
 };
 
@@ -163,5 +165,6 @@ window.toggleCart = toggleCart;
 window.removeFromCart = removeFromCart;
 window.sendToWhatsApp = sendToWhatsApp;
 window.toggleMobileMenu = () => document.getElementById('mobileMenu').classList.toggle('active');
+
 
 
